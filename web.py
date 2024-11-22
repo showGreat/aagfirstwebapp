9e7c3cc52c95ee95a4112be0d71eabf985a88995
aminu <siramin.aa@gmail.com>
2024-11-22 08:26:38 +0100

    Initial commit2

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 
 todos = functions.get_todos()
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"] +"\n"
@@ -12,6 +13,7 @@ def add_todo():
 
 st.title("MY TO DO APP")
 st.subheader("Welcome to my To Do App")
+st.text_input(label="Enter a Todo",placeholder="Enter a Todo",on_change=add_todo,key='new_todo')
 
 for index,todo in enumerate(todos) :
     checkbox = st.checkbox(todo,key=todo)
@@ -21,4 +23,3 @@ for index,todo in enumerate(todos) :
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="Enter a Todo",placeholder="Enter a Todo",on_change=add_todo,key='new_todo')
